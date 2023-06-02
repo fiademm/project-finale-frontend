9f1402130d8504a853d07c2c09b2b79e1efddffd
fiademm <fiademm1@gmail.com>
2023-06-02 19:52:22 +0000

    Update CourseCard component

--- src/App/components/CouseCard.js
+++ src/App/components/CouseCard.js
@@ -1,26 +1,74 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-const CourseCard = ({ course }) => {
-  const [progress, setProgress] = useState(0);
+// const CourseCard = ({ course }) => {
+//   const [progress, setProgress] = useState(0);
 
-  const handleEnroll = () => {
-    // TODO: Enroll the user in the course
+//   const handleEnroll = () => {
+//     // TODO: Enroll the user in the course
+//   };
+
+//   const handleProgressChange = (e) => {
+//     setProgress(e.target.value);
+//   };
+
+//   return (
+//     <div className="course-card">
+//       <h2>{course.name}</h2>
+//       <p>{course.description}</p>
+//       <div className="progress">
+//         <progress value={progress} max={5} />
+//       </div>
+//       <button onClick={handleEnroll}>Enroll</button>
+//     </div>
+//   );
+// };
+
+// export default CourseCard;
+
+
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function CourseCard({ course }) {
+  const [isEnrolled, setIsEnrolled] = useState(false);
+
+  const handleEnroll = async () => {
+    try {
+      await axios.post(
+        '/users/enroll',
+        { courseId: course.id },
+        { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }
+      );
+      setIsEnrolled(true);
+    } catch (error) {
+      console.error(error);
+      alert('Failed to enroll in course');
+    }
   };
 
-  const handleProgressChange = (e) => {
-    setProgress(e.target.value);
-  };
+  useEffect(() => {
+    const checkEnrollmentStatus = async () => {
+      try {
+        const response = await axios.get(`/courses/${course.id}`, {
+          headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+        });
+        setIsEnrolled(true);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    checkEnrollmentStatus();
+  }, []);
 
   return (
-    <div className="course-card">
-      <h2>{course.name}</h2>
+    <div>
+      <h2>{course.title}</h2>
       <p>{course.description}</p>
-      <div className="progress">
-        <progress value={progress} max={5} />
-      </div>
-      <button onClick={handleEnroll}>Enroll</button>
+      <img src={course.thumbnail_url} alt={`Thumbnail for ${course.title}`} />
+      {!isEnrolled && <button onClick={handleEnroll}>Enroll</button>}
     </div>
   );
-};
+}
 
 export default CourseCard;
