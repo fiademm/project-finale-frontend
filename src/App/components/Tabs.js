8617f36fe48c47752f37ca69fa6666439bac9dcb
fiademm <fiademm1@gmail.com>
2023-06-02 19:32:59 +0000

    Create and update tabs component

--- src/App/components/Tabs.js
+++ src/App/components/Tabs.js
@@ -1,38 +1,37 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { ActiveTabButton, TabButton } from './Buttons';
 
-const Tabs = ({ tabs }) => {
-    const [activeTabIndex, setActiveTabIndex] = useState(0);
+const TabOne = (props) => {
+  return (
+    <div className='tabs-component-container'>
+      <ActiveTabButton buttonText='UI/UX Design' buttonLink='/designpath' />
+      <TabButton buttonText='Mobile Development' buttonLink='/mobilepath' />
+      <TabButton buttonText='Web Development' buttonLink='/webpath' />
+    </div>
+  );
+};
 
-    const handleTabClick = (index) => {
-        setActiveTabIndex(index);
-    };
-    
-    return(
-        // <div className='tabs-component-container'>
-        //    <TabButton className="tab-button" tabText='UI/UX Designer' tabLink='' iconSource='' hoveredIconSource=''>UI/UX Design</TabButton>
-        //    <TabButton className="tab-button" tabText='Mobile Development' tabLink='' iconSource='' hoveredIconSource=''>Mobile Development</TabButton>
-        //    <TabButton className="tab-button" tabText='Web Development' tabLink='' iconSource='' hoveredIconSource=''>Web Development</TabButton>
-        // </div>
+const TabTwo = (props) => {
+  return (
+    <div className='tabs-component-container'>
+      <TabButton buttonText='UI/UX Design' buttonLink='/designpath' />
+      <ActiveTabButton buttonText='Mobile Development' buttonLink='/mobilepath' />
+      <TabButton buttonText='Web Development' buttonLink='/webpath' />
+    </div>
+  );
+};
 
-        <div>
-            <ul>
-                {tabs.map((tab, i) => (
-                <li key={i} onClick={() => handleTabClick(i)}>
-                    {tab}
-                    {activeTabIndex === i && <span style={{ textDecoration: "underline" }} />}
-                </li>
-                ))}
-            </ul>
-            {tabs.map((tab, i) => (
-                <div key={i} style={{ display: i === activeTabIndex ? "block" : "none" }}>
-                <h2>{tab}</h2>
-                <p>This is the content of the tab.</p>
-                </div>
-            ))}
-        </div>
-    );
+const TabThree = (props) => {
+  return (
+    <div className='tabs-component-container'>
+      <TabButton buttonText='UI/UX Design' buttonLink='/designpath' />
+      <TabButton buttonText='Mobile Development' buttonLink='/mobilepath' />
+      <ActiveTabButton buttonText='Web Development' buttonLink='/webpath' />
+    </div>
+  );
 };
 
 
-export default Tabs;
+
+export { TabOne, TabTwo, TabThree };
