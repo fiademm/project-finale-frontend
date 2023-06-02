dfd0e259cf9c6c0f3f0ace143fb93772203d8016
fiademm <fiademm1@gmail.com>
2023-06-02 19:35:11 +0000

    Update App file and styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Error500 from './App/components/Error500';
 import LandingPage from './App/components/LandingPage';
 import LectureVideo from './App/components/LectureVideo';
 import Login from './App/components/Login';
+import DesignPath from './App/components/Paths/DesignPath';
+import MobilePath from './App/components/Paths/MobilePath';
+import WebPath from './App/components/Paths/WebPath';
 import Register from './App/components/Register';
 import './App/styles/general.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -17,6 +20,9 @@ function App() {
           <Route path='/' element={<LandingPage />}></Route>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/register' element={<Register />}></Route>
+          <Route path='/designpath' element={<DesignPath />}></Route>
+          <Route path='/mobilepath' element={<MobilePath />}></Route>
+          <Route path='/webpath' element={<WebPath />}></Route>
           <Route path='/courses' element={<AvailableCourses />}></Route>
           <Route path='/video' element={<LectureVideo />}></Route>
           <Route path='/404' element={<Error404 />}></Route>
--- src/App/components/AvailableCourses.js
+++ src/App/components/AvailableCourses.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { HeaderAfterLogin } from './Headers';
 import { FooterAfterLogin } from './Footers';
-import Tabs from './Tabs';
+import Tabs, { TabOne } from './Tabs';
 
 const AvailableCourses = () => {
-    const tabs = ["UI Design", "Mobile Development", "Web Development"];
-
     return(
         <>
             <HeaderAfterLogin />
             <section className="component-container">
-                <Tabs tabs={tabs} />
                 <section className='courses-component-container'>
-
+                    <TabOne />
                 </section>
             </section>
             <FooterAfterLogin />
