fd52f48ea95a114f114242c5ddb9dbcbc3dcd5d2
JerryAgbo <jagbofoatsi@st.ug.edu.gh>
2023-06-06 17:22:54 -0700

    process

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import AvailableCourses from './App/components/AvailableCourses';
 import Error404 from './App/components/Error404';
 import Error500 from './App/components/Error500';
 import Home from './App/components/Home';
 import LandingPage from './App/components/LandingPage';
 import Login from './App/components/Login';
-import DesignPath from './App/components/Paths/DesignPath';
-import MobilePath from './App/components/Paths/MobilePath';
-import WebPath from './App/components/Paths/WebPath';
-import Register from './App/components/Register';
 import './App/styles/general.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
@@ -22,7 +17,6 @@ function App() {
         <Route path="/home" element={<Home />} />
           <Route path='/landingpage' element={<LandingPage />}></Route>
           <Route path='/login' element={<Login />}></Route>
-          <Route path='/register' element={<Register />}></Route>
           <Route path='/404' element={<Error404 />}></Route>
           <Route path='/500' element={<Error500 />}></Route>
         </Routes>
