a3d0bdc533e696348219ebe093e7af494fb92472
fiademm <fiademm1@gmail.com>
2023-06-10 22:31:38 +0000

    Update course details component

--- src/App/components/pages/CourseDetails.js
+++ src/App/components/pages/CourseDetails.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { LoggedInHeader } from '../Headers';
+import { LoggedInFooter } from '../Footers';
 
 const CourseDetails = () => {
-    return(
-        <></>
+    const location = useLocation();
+    const { videos } = location.state;
+
+    return (
+        <>
+            <LoggedInHeader />
+            <div className="card-deck">
+                {videos.map((video, index) => (
+                <div key={index} className="card">
+                    <div className="card-body">
+                    <h5 className="card-title">Video {index + 1}</h5>
+                    <p className="card-text">{video}</p>
+                    </div>
+                </div>
+                ))}
+            </div>
+            <LoggedInFooter />
+        </>
     );
-};
+  };
 
 export default CourseDetails;
