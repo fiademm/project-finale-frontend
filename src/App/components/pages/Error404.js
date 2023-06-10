cc5c43d3bd8dcc79305621b18929f239039fec8b
fiademm <fiademm1@gmail.com>
2023-06-10 22:32:45 +0000

    Update error components

--- src/App/components/pages/Error404.js
+++ src/App/components/pages/Error404.js
@@ -4,6 +4,7 @@ import Error404Img from "../../illustrations/404 Error.png";
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { LoggedInHeader } from '../Headers';
 import { LoggedInFooter } from '../Footers';
+import { FilledButton } from '../Buttons';
 
 const Error404 = () => {
     return(
@@ -12,7 +13,7 @@ const Error404 = () => {
                 <LoggedInHeader />
                 <div className="error-content-container">
                     <img className='error-illustration' src={Error404Img} alt="Page Not Found" />
-                    <Link to='/' ><button>Go back home</button></Link>
+                    <Link to='/' ><FilledButton buttonText='Go back home' /></Link>
                 </div>
                 <LoggedInFooter />
             </div>
--- src/App/components/pages/Error500.js
+++ src/App/components/pages/Error500.js
@@ -4,6 +4,7 @@ import Error500Img from "../../illustrations/500 Internal Server Error.png";
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { LoggedInFooter } from '../Footers';
 import { LoggedInHeader } from '../Headers';
+import { FilledButton } from '../Buttons';
 
 const Error500 = () => {
     return(
@@ -12,7 +13,7 @@ const Error500 = () => {
                 <LoggedInHeader />
                 <div className="error-content-container">
                     <img className='error-illustration' src={Error500Img} alt="Internal Server Error" />
-                    <Link to='/' ><button>Go back home</button></Link>
+                    <Link to='/' ><FilledButton buttonText='Go back home' /></Link>
                 </div>
                 <LoggedInFooter />
             </div>
