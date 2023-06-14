ef5ecc4ace488352bee387bdbfe7fa180aaa4271
JerryAgbo <jagbofoatsi@st.ug.edu.gh>
2023-06-14 15:02:56 -0700

    onwards

--- src/App/components/HeroSectionTwo.js
+++ src/App/components/HeroSectionTwo.js
@@ -4,37 +4,40 @@ import stats from './images/stats.jpg';
 
 function HeroSectionTwo() {
   return (
-    <div style={{ backgroundColor: '#777777' }}>
+    <div >
       <h1 className="title">Popular Courses</h1>
       <p className="description">
         Education is the process of learning knowledge, strengthening one’s <br />
         decision-making abilities, and getting ready to live a normal life.
       </p>
-      <img src={stats} alt="Stats" className="stats-image" />
-      <div className="course-details">
-        <h2 className="course-title">Frontend Development - Advanced</h2>
-        <p className="descriptionOne">
-        This course is designed for beginners who want to  <br />
-        learn the basics of frontend development. You will  <br />
-        learn the fundamentals of HTML, CSS, and JavaScript, <br /> 
-        and you will build your first web page. By the end of 
-      </p>
-      <button className="view-course-button">View Course</button>
-      </div>
+      
+      <div className="course-container">
+  <div className='first-container'>
+    <img src={stats} alt="Stats" className="stats-image" />
+    <div className="course-details">
+      <h2 className="course-title">Frontend Development - Advanced</h2>
+      <p className="descriptionOne">
+      This course is designed for beginners who want to  <br />
+      learn the basics of frontend development. You will  <br />
+      learn the fundamentals of HTML, CSS, and JavaScript.
+    </p>
+    <button className="view-course-button">View Course</button>
+    </div>
+  </div>
 
-   <div className='big-container'>
-      <img src={stats} alt="Stats" className="stats-imageOne" />
-      <div className="course-detailsOne">
-        <h2 className="course-titleOne">Frontend Development - Advanced</h2>
-        <p className="descriptionTwo">
-        This course is designed for beginners who want to  <br />
-        learn the basics of frontend development. You will  <br />
-        learn the fundamentals of HTML, CSS, and JavaScript, <br /> 
-        and you will build your first web page. By the end of 
-      </p>
-      <button className="view-course-buttonOne">View Course</button>
-      </div>
-      </div>
+  <div className='second-container'>
+    <img src={stats} alt="Stats" className="stats-imageOne" />
+    <div className="course-detailsOne">
+      <h2 className="course-titleOne">Frontend Development - Advanced</h2>
+      <p className="descriptionTwo">
+      This course is designed for beginners who want to  <br />
+      learn the basics of frontend development. You will  <br />
+      learn the fundamentals of HTML, CSS, and JavaScript.
+    </p>
+    <button className="view-course-buttonOne">View Course</button>
+    </div>
+  </div>
+</div>
    </div>
   );
 }
