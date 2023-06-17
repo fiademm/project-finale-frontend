e17c191e4af7ebf24f5f448548e07be8e731e2e0
fiademm <fiademm1@gmail.com>
2023-06-17 10:38:54 +0000

    Update profile screen

--- src/App/components/pages/Profile.js
+++ src/App/components/pages/Profile.js
@@ -1,8 +1,51 @@
 import React from 'react';
+import Circle from './Circle';
+import { LoggedInHeader } from '../Headers';
+import { LoggedInFooter } from '../Footers';
+import '../../styles/components.css';
+import { LinksList } from '../CardsList';
+import { BsAward, BsBookmark, BsPlayBtn, BsTrophy } from "react-icons/bs";
 
 const Profile = () => {
+    const nameOfUser = 'Otchere Fia Israel';
+    const emailAddress = 'fiademm1@gmail.com';
+
+    const links = [
+        {
+            icon: BsTrophy,
+            title: 'Achievements',
+            link: '/achievements'
+        },
+        {
+            icon: BsBookmark,
+            title: 'Bookmarks',
+            link: '/bookmarks'
+        },
+        {
+            icon: BsAward,
+            title: 'Certificates',
+            link: '/certificates'
+        },
+        {
+            icon: BsPlayBtn,
+            title: 'My courses',
+            link: '/mycourses'
+        }
+    ]
+
     return(
-        <></>
+        <>
+            <LoggedInHeader />
+            <div className="component-container">
+                <Circle />
+                <section>
+                    <p style={{fontFamily: 'Jost-Medium', fontSize: '1.1vw'}}>{nameOfUser}</p>
+                    <p style={{fontSize: '0.83vw'}}>{emailAddress}</p>
+                </section>
+                <LinksList links={links} />
+            </div>
+            <LoggedInFooter />
+        </>
     );
 };
 
