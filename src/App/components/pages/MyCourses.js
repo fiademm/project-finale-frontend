f4d73c6885d0f7d9cf967e698f1a8e897da9da3b
fiademm <fiademm1@gmail.com>
2023-07-15 13:55:03 +0000

    Update mycourses component

--- src/App/components/pages/MyCourses.js
+++ src/App/components/pages/MyCourses.js
@@ -6,25 +6,6 @@ import { CourseList, EnrolledCourseList } from '../CardsList';
 const MyCourses = () => {
     const enrolledCourses = [
         {
-          title: "Intro to Python",
-          description: "Learn the basics of Python programming with this introductory course.",
-          thumbnail: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-          numVideos: 10,
-          rating: 4.5,
-          videos: [
-            "https://www.youtube.com/watch?v=_Z1t7MNk0c4",
-            "https://www.youtube.com/watch?v=kqtD5dpn9C8",
-            "https://www.youtube.com/watch?v=QXeEoD0pB3E",
-            "https://www.youtube.com/watch?v=6tNS--WetLI",
-            "https://www.youtube.com/watch?v=rvvN6BQ5mVM",
-            "https://www.youtube.com/watch?v=Kg1Yvry_Ydk",
-            "https://www.youtube.com/watch?v=0Rryi19X_Y4",
-            "https://www.youtube.com/watch?v=Z1Yd7upQsXY",
-            "https://www.youtube.com/watch?v=QfQE1ayCzf8",
-            "https://www.youtube.com/watch?v=JGwWNGJdvx8"
-          ]
-        },
-        {
           title: "React Fundamentals",
           description: "Get started with the React JavaScript library in this introductory course.",
           thumbnail: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
@@ -44,30 +25,6 @@ const MyCourses = () => {
             "https://www.youtube.com/watch?v=5J6fs_BlVC0",
             "https://www.youtube.com/watch?v=ZnRFerIP8aA"
           ]
-        },
-        {
-          title: "Machine Learning",
-          description: "Learn how to build machine learning models with Python and TensorFlow.",
-          thumbnail: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-          numVideos: 15,
-          rating: 4.0,
-          videos: [
-            "https://www.youtube.com/watch?v=aircAruvnKk",
-            "https://www.youtube.com/watch?v=tPYj3fFJGjk",
-            "https://www.youtube.com/watch?v=IHZwWFHWa-w",
-            "https://www.youtube.com/watch?v=HGgdcKbC5ro",
-            "https://www.youtube.com/watch?v=KNAWp2S3w94",
-            "https://www.youtube.com/watch?v=4vryPwLtjIY",
-            "https://www.youtube.com/watch?v=0Lt9w-BxKFQ",
-            "https://www.youtube.com/watch?v=6g4O5UOH304",
-            "https://www.youtube.com/watch?v=2FmcHiLCwTU",
-            "https://www.youtube.com/watch?v=5DknTFbcGVM",
-            "https://www.youtube.com/watch?v=4qxpOYv1LBE",
-            "https://www.youtube.com/watch?v=JLNrR6HlN4Q",
-            "https://www.youtube.com/watch?v=tNa99PG8hR8",
-            "https://www.youtube.com/watch?v=7eh4d6sabA0",
-            "https://www.youtube.com/watch?v=I74ymkoNTnw"
-          ]
         }
       ];
 
@@ -75,11 +32,8 @@ const MyCourses = () => {
         <>
             <LoggedInHeader />
             <section className='list-component-container'>
-                <section style={{backgroundColor: '#333'}}>
-                    <h2 style={{textAlign: 'left', background: 'none', fontFamily: 'Jost-SemiBold', display: 'flex', flexDirection: 'column', gap: '0.5vw'}}>Enrolled courses <hr /></h2>
-
-                </section>
-                <EnrolledCourseList enrolledCourses={enrolledCourses} />
+              <h2 style={{textAlign: 'left', background: 'none', fontFamily: 'Jost-SemiBold', display: 'flex', flexDirection: 'column', gap: '0.5vw'}}>My learning <hr /></h2>
+              <EnrolledCourseList enrolledCourses={enrolledCourses} />
             </section>
             <LoggedInFooter />
         </>
